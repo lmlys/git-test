6834dd1833c17a4be8c2de4f833778317dcaf92a
zhangsan <zhangsan@itcast.cn>
2019-01-22 19:04:24 +0800

    lalla

---
+++ src/com/itheima/test/Git.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
